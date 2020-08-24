3b19fb00e0f5c13686cbc865b0781cc10ddbf532
1244961851@qq.com <1997529li>
2020-08-24 14:26:59 +0800

    should_get_the_first_locker_ticket_when_store_bag_by_manager_given_the_first_locker_has_the_more_available_capacity

---
+++ src/test/java/LockerRobotManagerTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class LockerRobotManagerTest {
+    @Test
+    public void should_get_the_first_locker_ticket_when_store_bag_by_manager_given_the_first_locker_has_the_more_available_capacity() {
+        Locker locker1 = new Locker(2);
+        Locker locker2 = new Locker(1);
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2));
+        Bag bag = new Bag();
+        Ticket ticket = lockerRobotManager.store(bag);
+        assertNotNull(ticket);
+        assertEquals(bag, locker1.pickUpBy(ticket));
+    }
+}
