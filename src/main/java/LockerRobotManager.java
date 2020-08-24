a67d3350ae92c4ec0cf74f5d21376bb3eedcac5b
1244961851@qq.com <1997529li>
2020-08-24 16:01:14 +0800

    should_store_the_bag_by_the_smart_robot_when_store_bag_by_robot_given_the_first_robot_is_smart_and_available

--- src/main/java/LockerRobotManager.java
+++ src/main/java/LockerRobotManager.java
@@ -2,13 +2,18 @@ import java.util.List;
 
 public class LockerRobotManager {
 
+    private List<Object> lockerRobotManagers;
     private List<Locker> lockers;
 
-    public LockerRobotManager(List<Locker> lockers) {
+    public LockerRobotManager(List<Locker> lockers, List<Object> lockerRobotManagers) {
+        this.lockerRobotManagers = lockerRobotManagers;
         this.lockers = lockers;
     }
 
     public Ticket store(Bag bag) {
+        if(lockerRobotManagers!=null){
+            return ((SmartLockerRobot)lockerRobotManagers.get(0)).store(bag);
+        }
         int maxAvailableCapacity = 0;
         Locker maxAvailableCapacityLocker = null;
         for(Locker locker : lockers){
--- src/test/java/LockerRobotManagerTest.java
+++ src/test/java/LockerRobotManagerTest.java
@@ -9,7 +9,7 @@ public class LockerRobotManagerTest {
     public void should_get_the_first_locker_ticket_when_store_bag_by_manager_given_the_first_locker_has_the_more_available_capacity() {
         Locker locker1 = new Locker(2);
         Locker locker2 = new Locker(1);
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2),null);
         Bag bag = new Bag();
         Ticket ticket = lockerRobotManager.store(bag);
         assertNotNull(ticket);
@@ -20,7 +20,7 @@ public class LockerRobotManagerTest {
     public void should_get_the_second_locker_ticket_when_store_bag_by_manager_given_the_second_locker_has_the_more_available_capacity() {
         Locker locker1 = new Locker(1);
         Locker locker2 = new Locker(2);
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2),null);
         Bag bag = new Bag();
         Ticket ticket = lockerRobotManager.store(bag);
         assertNotNull(ticket);
@@ -31,7 +31,7 @@ public class LockerRobotManagerTest {
     public void should_get_the_first_locker_ticket_when_store_bag_by_manager_given_two_lockers_have_the_same_available_capacity() {
         Locker locker1 = new Locker(1);
         Locker locker2 = new Locker(1);
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2),null);
         Bag bag = new Bag();
         Ticket ticket = lockerRobotManager.store(bag);
         assertNotNull(ticket);
@@ -44,8 +44,23 @@ public class LockerRobotManagerTest {
         locker1.store(new Bag());
         Locker locker2 = new Locker(1);
         locker2.store(new Bag());
-        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker1, locker2),null);
         Bag bag = new Bag();
         assertThrows(LockerFullException.class,()->lockerRobotManager.store(bag));
+    }
+
+    @Test
+    public void should_store_the_bag_by_the_smart_robot_when_store_bag_by_robot_given_the_first_robot_is_smart_and_available(){
+        Locker locker1 = new Locker(1);
+        Locker locker2 = new Locker(1);
+        Locker locker3 = new Locker(1);
+        Locker locker4 = new Locker(1);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(locker1, locker2));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(locker3, locker4));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(null,Arrays.asList(smartLockerRobot, primaryLockerRobot));
+        Bag bag = new Bag();
+        Ticket ticket = lockerRobotManager.store(bag);
+        assertNotNull(ticket);
+        assertEquals(bag,smartLockerRobot.pickUpBy(ticket));
     }
 }
